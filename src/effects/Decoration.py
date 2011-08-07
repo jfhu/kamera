6899339c9f75df783c124870c62ab7657914704b
Youyou Yang <superyoyoy@gmail.com>
2011-08-07 15:07:52 -0700

    add decoration effects

---
+++ src/effects/Decoration.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from KameraEffectBase import *
+from PIL import Image
+
+class Decoration(KameraEffectBase):
+    def get_name(cls):
+        return "Decoration"
+    
+    def get_description(cls):
+        return "Add decoration image to video"
+    
+    def process_image(cls, image, options):
+        dec = Image.open('../image/mustache.png')
+        return cls.addImages(image, dec,[(100, 100), (220, 130)])
+        
+    def addImages(cls, image, dec, positions):
+        for (x, y) in positions:
+            cls.addImage(image, dec, (x, y))
+        return image
+        
+    def addImage(cls, image, dec, p):
+        image.paste(dec, p, dec)
+        return image
+        
+        
